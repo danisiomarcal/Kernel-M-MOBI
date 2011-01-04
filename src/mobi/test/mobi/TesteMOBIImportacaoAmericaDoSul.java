42211a843b2702d9648230ed6566b300ed5ff353
Danisio <danisiomarcal@gmail.com>
2011-01-03 21:15:38 -0300

    Alteracao na classe mobi2owl para importacao das relacoes de heranca, equivalencia, classes e instancias

--- src/mobi/test/mobi/TesteMOBIImportacaoAmericaDoSul.java
+++ src/mobi/test/mobi/TesteMOBIImportacaoAmericaDoSul.java
@@ -6,6 +6,8 @@ import mobi.core.Mobi;
 import mobi.core.concept.Class;
 import mobi.core.concept.Instance;
 import mobi.core.relation.CompositionRelation;
+import mobi.core.relation.EquivalenceRelation;
+import mobi.core.relation.InheritanceRelation;
 import mobi.core.relation.InstanceRelation;
 import mobi.extension.export.owl.Mobi2OWL;
 
@@ -20,7 +22,7 @@ public class TesteMOBIImportacaoAmericaDoSul {
 		try
 		{
 			Mobi2OWL mobiOWL = new Mobi2OWL();
-			mobiOWL.importForMobiOfOWL("C:\\BaseOntologia\\ronaldo@reconcavotecnologia_org_br_comgresso.owl");
+			mobiOWL.importForMobiOfOWL("C:\\BaseOntologia\\fpbitencourt-congresso.owl");
 			Mobi mobi = mobiOWL.getMobi();
 			
 			for(Class mobiClass : mobi.getAllClasses().values())
@@ -32,6 +34,39 @@ public class TesteMOBIImportacaoAmericaDoSul {
 					System.out.println(instance.getUri());
 				}
 				System.out.println("==================End of Class: " + mobiClass.getUri().toUpperCase() + "==================");
+			}
+			
+			for(InheritanceRelation inheritance : mobi.getAllInheritanceRelations().values())
+			{
+				System.out.println("==================Reading Relation==================");
+				System.out.println("SUPER CLASS: " + inheritance.getClassA().getUri());
+				System.out.println("SUB CLASS: " + inheritance.getClassB().getUri());
+				System.out.println("NAME: " + inheritance.getUri());
+				
+				System.out.println("Reading Relations between instances");
+				for(InstanceRelation instanceRelation : inheritance.getInstanceRelationMapA().values())
+				{
+					System.out.println("Instance " + instanceRelation.getInstance().getUri().toUpperCase() + " has values: ");
+					for(Instance instance: instanceRelation.getAllInstances().values())
+						System.out.println(instance.getUri().toUpperCase());
+				}
+				System.out.println("==================End of Relation==================");
+			}
+			
+			for(EquivalenceRelation equivalence : mobi.getAllEquivalenceRelations().values())
+			{
+				System.out.println("==================Reading Relation==================");
+				System.out.println("CLASS A: " + equivalence.getClassA().getUri());
+				System.out.println("CLASS B: " + equivalence.getClassB().getUri());
+				System.out.println("NAME: " + equivalence.getUri());
+				System.out.println("Reading Relations between instances");
+				for(InstanceRelation instanceRelation : equivalence.getInstanceRelationMapA().values())
+				{
+					System.out.println("Instance " + instanceRelation.getInstance().getUri().toUpperCase() + " has values: ");
+					for(Instance instance: instanceRelation.getAllInstances().values())
+						System.out.println(instance.getUri().toUpperCase());
+				}
+				System.out.println("==================End of Relation==================");
 			}
 			
 			for(CompositionRelation composition : mobi.getAllCompositionRelations().values())
